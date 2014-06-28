acdb984c2909e0012038c5b19d87b959d9dcd103
Zach Langer <zacho19@gmail.com>
2014-06-27 19:36:26 -0600

    ch2 fixes

--- ch_2/ex2.rb
+++ ch_2/ex2.rb
@@ -1,9 +1,9 @@
 # A comment, is so you can read your program later.
 # Anything after the # is ignored by ruby.
 
-puts "I could have code like this." # and the comment after is ignored
+puts 'I could have code like this.' # and the comment after is ignored
 
 # You can also use a comment to "disable" or comment out a piece of code:
 # puts "this wont run"
 
-puts "this will run."
+puts 'this will run.'
