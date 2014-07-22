2773659a7a2ae03dbc27563dba5bec2c96f8fb36
Zach Langer <zacho19@gmail.com>
2014-07-21 21:35:07 -0600

    chapters 3-5 changes

--- ch_5/ex5.rb
+++ ch_5/ex5.rb
@@ -1,19 +1,39 @@
-# establishing variables
-name = 'Zed A. Shaw'
-age = 35 # not a lie
-height = 74 # inches
-weight = 180 # lbs
-eyes = 'Blue'
-teeth = 'White'
-hair = 'Brown'
+# establishing name
+my_name = 'Zed A. Shaw'
+# establishing age
+my_age = 35 # not a lie
+# establishing height
+my_height = 74 # inches
+# establishing weight
+my_weight = 180 # lbs
+# establishing eye color
+my_eyes = 'Blue'
+# establishing teeth status
+my_teeth = 'White'
+# establishing hair color
+my_hair = 'Brown'
+# establishing weight in kilos
+my_weight_in_kilos = weight * .784
+# establishing height in cm
+my_height_in_cm = height * 2.65
 # these %d's are known as formatting sequences
-puts "Let's talk about %s." % name
-puts "He's %d inches tall." % height
-puts "He's %d pounds heavy." % weight
+# printing his name
+puts "Let's talk about %s." % my_name
+# printing his height
+puts "He's %d inches tall." % my_height
+# printing his weight
+puts "He's %d pounds heavy." % my_weight
+# printing a random string
 puts "Actually that's not too heavy."
-puts "He's got %s eyes and %s hair." % [eyes, hair]
-puts "His teeth are usually %s depending on the coffee." % teeth
+# printing his hair and eye color
+puts "He's got %s eyes and %s hair." % [my_eyes, my_hair]
+# printing his teeth status
+puts "His teeth are usually %s depending on the coffee." % my_teeth
+# printing his weight in kilos
+puts "He's %d kilos heavy." % my_weight_in_kilos
+# printing his height
+puts "He's %d cm tall." % my_height_in_cm
 # this line is tricky, try to get it exactly right
-puts "If I add %d, %d, and %d I get %d." % [age, height, weight, age + height + weight]
-
-puts "hair = %s, eyes = %s, teeth = %s" % [hair, eyes, teeth]
+puts "If I add %d, %d, and %d I get %d." % [my_age, my_height, my_weight, my_age + my_height + my_weight]
+# printing hair eyes and teeth all on one line
+puts "hair = %s, eyes = %s, teeth = %s" % [my_hair, my_eyes, my_teeth]
--- ch_5/my_ex5.rb
+++ ch_5/my_ex5.rb
@@ -1,20 +1,37 @@
+# establishing name
 name = 'Zed A. Shaw'
+# establishing age
 age = 35 # not a lie
+# establishing height
 height = 74 # inches
+# establishing weight
 weight = 180 # lbs
+# establishing eye color
 eyes = 'Blue'
+# establishing teeth status
 teeth = 'White'
+# establishing hair color
 hair = 'Brown'
-weight_in_kilos = weight * 0.453592
-height_in_cm = height * 2.54
-
-# these %d's are known as formating sequences
+# these %d's are known as formatting sequences
+weight_in_kilos = weight * .785
+height_in_cm = height * 2.65
+# printing his name
 puts "Let's talk about %s." % name
+# printing his height
 puts "He's %d inches tall." % height
+# printing his weight
 puts "He's %d pounds heavy." % weight
+# printing a random string
 puts "Actually that's not too heavy."
+# printing his hair and eye color
 puts "He's got %s eyes and %s hair." % [eyes, hair]
+# printing his teeth status
 puts "His teeth are usually %s depending on the coffee." % teeth
-
+# printing his weight in kilos
+puts "He's %d kilos heavy." % weight_in_kilos
+# printing his height
+puts "He's %d cm tall." % height_in_cm
 # this line is tricky, try to get it exactly right
 puts "If I add %d, %d, and %d I get %d." % [age, height, weight, age + height + weight]
+# printing hair eyes and teeth all on one line
+puts "hair = %s, eyes = %s, teeth = %s" % [hair, eyes, teeth]
